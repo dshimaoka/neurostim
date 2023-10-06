8483679aa04c46a3a2480967fbe89da1da1187d8
Daisuke Shimaoka <sageagainstthemachine@gmail.com>
2023-10-07 00:25:55 +1100

    Add files via upload
    as of 7/10/2023 from MU00175834

---
+++ movie.m
@@ -0,0 +1,345 @@
+classdef movie < neurostim.stimulus
+    % Stimulus class to present movies as textures (without using
+    % GStreamer, which does not play nice with the Matlab JVM).
+    %
+    % This class uses the VideoReader to read the movie files and convert
+    % to OpenGL textures in the interrial interval. For a 1 MB movie (20
+    % seconds at 30 fps), this takes about 4 seconds -= so a long ITI
+    % should be expected. With some additional bookkeeping this could be
+    % pushed to the beforeExperiment phase (to load multiple movies to
+    % texture memory at the start of the experiment)
+    %
+    % 
+    % Settable properties:
+    %   id      - id(s) of the texture(s) to show on
+    %             the next frame
+    %   width   - width on screen (screen units)
+    %   hight   - height on screen (screen units)
+    %   X
+    %   Y
+    %   orientation - in degrees.
+    %   filename  - full path to the movie file. This must be one of the
+    %   formats that VideoReader can interpret.
+    %   path - If filename is not a full path, specify a path separately
+    %   here.
+    % 
+    %   framerate - the framerate at which to play the Movie (defaults to the rate in the file)
+    %   filterMode - Texture filterModes - 1 = bilinear, the default.  For higher quality, increase the number (up to 5). See Screen('DrawTexture?') for details.  
+    %   loop - Loop the movie within a trial.
+    %
+    % Read only properties   
+    %   nrFrames = nmber of frames in the movie
+    %   xPixels , yPixels native number of pixels in the movie file.
+    % 
+    %  BK - May 2019
+    
+    properties (Access = private)
+        % Each frame will be associated with a texture
+        tex = [];       % Vector fo texture pointers 
+        rect =[];       % Rect to apply the texture
+        currentlyLoadedFile = '';  % Full path to currently loaded file.
+    end
+    
+     properties (GetAccess = public, SetAccess = private)
+        fracFrameCntr;      % Current fractional frame cntr.
+    end
+    
+    methods (Access = public)
+        function o = movie(c,name)
+            o = o@neurostim.stimulus(c,name);
+            o.addProperty('width',[],'validate',@isnumeric);
+            o.addProperty('height',[],'validate',@isnumeric);
+            o.addProperty('xPixels',[],'validate',@isnumeric, 'sticky',true);
+            o.addProperty('yPixels',[],'validate',@isnumeric, 'sticky',true);
+            o.addProperty('nrFrames',[],'validate',@isnumeric, 'sticky',true);
+            o.addProperty('filename','','validate',@ischar);
+            o.addProperty('path','','validate',@(x) (ischar(x) && exist(x,'dir')));
+            o.addProperty('filterMode',1,'validate',@(x) ismember(x,[0:5]));
+            o.addProperty('framerate',[],'validate',@isnumeric, 'sticky',true);
+            o.addProperty('orientation',0,'validate',@isnumeric);
+            o.addProperty('loop',false,'validate',@islogical);
+             %record #frames of every trial
+            o.addProperty('nrFramesRecord', [], 'validate',@isnumeric);
+        end
+        
+        function afterTrial(o)
+            o.nrFramesRecord = o.frame; %smaller
+%             o.nrFramesRecord = o.cic.frame; %larger
+        end
+             
+        function afterExperiment(o)
+            % clean up the textures
+            Screen('Close',o.tex);
+            o.tex = [];
+        end
+        
+        function beforeTrial(o)
+            % Check what the current filename is, load it if necessary
+            if isempty(o.path)
+                file = o.filename;
+            else
+                file = fullfile(o.path,o.filename);
+            end
+            if strcmpi(o.currentlyLoadedFile,file)
+                % No need to reload
+            else
+                % Cleanup textures from the previous movie
+                Screen('Close',o.tex);
+                if ~exist(file,'file')
+                    error(['Movie file : ' file ' could not be found']);
+                end
+                try
+                    % tic
+                    % Use the VideoReader object to read each of the frames
+                    % and put it in texture memory.
+                    obj = VideoReader(file);
+                    o.xPixels = obj.Width;
+                    o.yPixels = obj.Height;                    
+                    if isempty(o.framerate)
+                        o.framerate = obj.FrameRate;
+                    end
+                    % create the textures
+                    i=0;
+                    while hasFrame(obj)
+                        i=i+1;
+                        o.tex(i) = Screen('MakeTexture',o.window,readFrame(obj));
+                    end
+                    o.nrFrames =i;
+                    % toc
+                catch me
+                    me.message
+                    error(['Could not load the movie : ' file]);
+                end
+                o.currentlyLoadedFile =file;
+            end
+            
+            % Determine the currently requested size of the movie
+            if isempty(o.width)
+                % Do not scale
+                o.width = o.cic.screen.width*o.xPixels/o.cic.screen.xpixels;
+            end
+            if isempty(o.height)
+                % Do not scale
+                o.height = o.cic.screen.height*o.yPixels/o.cic.screen.ypixels;
+            end
+            o.rect = 0.5*[-o.width o.height o.width -o.height];
+            o.fracFrameCntr=  1;% Reset the fractional frame cntr
+            
+        end
+        
+        function beforeFrame(o)
+            if isempty(o.tex); return; end % Nothing to do
+            % Increase the fractional frame cntr by some amount determined
+            % by the desired framerate and the actual (targeted) framerate
+            % of the application.
+            o.fracFrameCntr = o.fracFrameCntr+o.framerate/o.cic.screen.frameRate;
+            if o.loop
+                thisFrame = mod(floor(o.fracFrameCntr)-1,o.nrFrames)+1;
+            else
+                thisFrame = floor(o.fracFrameCntr);
+            end
+            
+            if thisFrame <= o.nrFrames
+                % These are not currently used/varied but could be added if neeed.
+                globalAlpha=1;modulateColor=[];textureShader=[];specialFlags=[];auxParameters=[];
+                Screen('DrawTextures',o.window,o.tex(thisFrame),[],o.rect,o.orientation,o.filterMode,globalAlpha,modulateColor,textureShader,specialFlags,auxParameters);
+            end
+        end
+        
+         function [frames_ori, frameIdx_final, timeVec, frames_final] = reconstructStimulus(o, varargin)
+            %Reconstruct the movie stimulus offline of a specified trial.
+            % INPUTS:
+            % trial: trial number
+            % moviePath: path to the original movie file. if empty, use the
+            % one in c.movie.filename
+            % replay: whether to show movie [default: false]
+            % replayFPS: frame rate of the replay movie [default: 60Hz]
+            %
+            % OUTPUTS;
+            % frames_ori: original movie 4D stack (y - x - color- frame)
+            % frameIdx_final: frame index for restoring frames_final =
+            % frame(:,:,:,frameIdx_final)
+            % timeVec: time vector corresponding to frames_final
+            % OPTIONAL OUTPUTS:
+            % frames_final: a 4D stack of images of all the screen
+            % refleshes
+            % created from neurostim/stimli/noiseclut.reconstructStimulus
+            
+            % TODO: save every N frames of the movie or cic.frame of every
+            % trials with addProperty()
+            
+            p=inputParser;
+            p.addParameter('replay',false);
+            p.addParameter('trial',1);
+            p.addParameter('moviePath',[]);
+            p.addParameter('replayFPS',60);%Display++ is 120Hz
+            p.parse(varargin{:});
+            p = p.Results;
+            
+            % width = get(c.movie.prms.width,'trial',p.trial, 'atTrialTime', Inf);
+            % height = get(c.movie.prms.height,'trial',p.trial, 'atTrialTime', Inf);
+            % nrFrames = get(o.prms.nrFrames,'trial',p.trial, 'atTrialTime', Inf);
+            %p.moviePath = get(o.prms.path,'trial',p.trial, 'atTrialTime', Inf);
+            filename = get(o.prms.filename,'trial',p.trial, 'atTrialTime', Inf);
+            % orientation = get(o.prms.orientation,'trial',p.trial, 'atTrialTime', Inf);
+            loop = get(o.prms.loop,'trial',p.trial, 'atTrialTime', Inf);
+            frameRate = get(o.prms.framerate,'trial',p.trial, 'atTrialTime', Inf);
+            refleshRate = o.cic.screen.frameRate; %screen reflesh rate
+            xPixels = get(o.prms.xPixels,'trial',p.trial, 'atTrialTime', Inf);
+            yPixels = get(o.prms.yPixels,'trial',p.trial, 'atTrialTime', Inf);
+            nrFramesRecord = get(o.prms.nrFramesRecord,'trial',p.trial, 'atTrialTime', Inf);
+            
+            
+            %Everything is in hand. Reconstruct.
+            %clutVals = cell(1,numel(p.trial));
+            warned = false;
+            if p.replay, figure; end
+            
+            %% beforeTrial
+            if isempty(p.moviePath)
+                file = filename;
+            else
+                [~,filename_p,filename_s] = fileparts(filename);
+                filename = [filename_p filename_s];
+                file = fullfile(p.moviePath,filename);
+            end
+            
+            obj = VideoReader(file);
+            frames_ori = uint8([]);%zeros(obj.Height, obj.Width, 3);
+            
+            i=0;
+            disp('loading movie');
+            while hasFrame(obj)
+                i=i+1;
+                frames_ori(:,:,:,i) = readFrame(obj);
+            end
+            nrFrames =i;
+            disp('done loading movie');
+            
+            clear obj ;
+            %% beforeFrame
+            %% temporally down sampling
+            frameIdx_ds = [];%<this defines length of frameIdx_final
+            frameCntr = 1;
+            thisFrame = 0;
+            iFrame = 1;
+            while (thisFrame < size(frames_ori,4))
+                frameCntr = frameCntr+frameRate...
+                    /refleshRate;
+                if loop
+                    thisFrame = mod(floor(frameCntr)-1,nrFrames)+1;
+                else
+                    thisFrame = floor(frameCntr);
+                end
+                frameIdx_ds(iFrame) = thisFrame;
+                iFrame = iFrame + 1;
+            end
+            
+            
+            %Use a figure window to show the reconstructed images
+            if p.replay
+                implay(frames_ori(:,:,:,frameIdx_ds), p.replayFPS);
+            end
+            
+            
+            %Up til here, we have reconstructed the unique images that were
+            %shown, in the right order, but not taken into account the
+            %update rate, nor dropped frames logged in CIC.
+            %
+            %So, our task here is to use repelem() to duplicate each image
+            %the right number of times to restore the actual time-line.
+            updateInterval = 1; %update at every screen reflesh
+            
+            %We need to take into account frame-drops. So gather info here
+            frDr = get(o.cic.prms.frameDrop,'trial',p.trial,'struct',true);
+            if size(frDr.block,1) < size(frDr.block,2)
+                frDr.block = frDr.block';%rmfield(frDr, 'block');
+            end
+            framesWereDropped = ~iscell(frDr.data);
+            if framesWereDropped
+                stay = ~isnan(frDr.data(:,1)); %frameDrop initialises to NaN
+                frDr = structfun(@(x) x(stay,:),frDr,'unif',false);
+                
+                %Convert duration of frame drop from ms to frames
+                % (this assumes frames were synced?)
+                % duration smaller than one frame is converted to one frame
+                frDr.data(:,2) = o.cic.ms2frames(1000*frDr.data(:,2));
+            end
+            
+            %Need to align the frame-drop data to the onset of this stimulus
+            %On what c.frame did the stimulus appear, and how long was it shown?
+            stimStart = get(o.prms.startTime,'trial',p.trial,'struct',true);
+            stimStop = get(o.prms.stopTime,'trial',p.trial,'struct',true);
+            %stimStop remains Inf if we stop an experiment prematurely via
+            %"escape". Fix that here.
+            [~,~,trialStopTime] = get(o.cic.prms.trialStopTime,'trial',p.trial);
+            ix = isinf(stimStop.trialTime);
+            stimStop.trialTime(ix) = trialStopTime(ix);
+            
+            %Calculate stimulus duration in display frames
+            %the line below is originally implemented in noiseclut.m:
+            stimDur_Fr = nrFramesRecord;
+            %stimDur_Fr = c.cic.ms2frames(stimStop.trialTime-stimStart.trialTime); <why
+             
+            %Initially assume no drops. i.e. all repeats were due to intended frame...
+            %interval and all repeats were shown (nothing guarantees that...
+            %could be mid-way through an interval when the stimulus/trial ends.
+            cbCtr = length(frameIdx_ds);
+            cbByFrame = repelem(1:cbCtr,updateInterval...
+                *ones(1,cbCtr));
+            
+            %Get the frame drop data for this trial
+            these = frDr.trial==p.trial;
+            thisFrDrData = frDr.data(these,:);
+            
+            if ~isempty(thisFrDrData)
+                
+                %Discard drops that happened before or after
+                kill = thisFrDrData(:,1)<stimStart.frame ...
+                    | thisFrDrData(:,1)>stimStop.frame ...
+                    | thisFrDrData(:,1)>cbCtr;
+                thisFrDrData(kill,:) = [];
+                
+                %Now re-number the frame drops relative to our first frame
+                thisFrDrData(:,1) = thisFrDrData(:,1) - stimStart.frame+1;
+                
+                %Now add in the repeats caused by dropped frames
+                framesPerFrame = ones(size(cbByFrame));
+                framesPerFrame(thisFrDrData(:,1)) = thisFrDrData(:,2)+1;
+                
+                cbByFrame = repelem(cbByFrame,framesPerFrame);
+            end
+            
+            %**** BAND-AID
+            if stimDur_Fr > numel(cbByFrame)
+                %Last frame of trial (screen clearing) must have been dropped! That one's not logged.
+                cbByFrame(end:stimDur_Fr) = cbByFrame(end); %Our last frame must have been shown for longer
+            end
+            %*****
+            
+            %Chop off any frames that were never shown due to end of stimulus
+            cbByFrame = cbByFrame(1:stimDur_Fr);
+            
+            %Timeline reconstructed, so use it to convert the length of clutVals to time
+            frameIdx_final = frameIdx_ds(cbByFrame);
+            if nargout>3
+                frames_final = frames_ori(:,:,:,frameIdx_final);
+            end
+            
+            timeVec = stimStart.trialTime:1e3/refleshRate:stimStop.trialTime;
+            timeVec = timeVec(1:end-1);%at stimStop.trialTime, frame is NOT presented
+            %timeVec = linspace(stimStart.trialTime, stimStop.trialTime, stimDur_Fr);
+            %timeVec = c.cic.frames2ms(1:size(frames_final,4));
+            
+            disp(['#images in the movie:' num2str(size(frames_ori,4))]);
+            disp(['#supposed frames wo dropped frames:' num2str(length(frameIdx_ds))]);
+            disp(['#dropped frames:' num2str(size(thisFrDrData,1))]);
+            disp(['#reconstructed frames:' num2str(length(frameIdx_final))]); %=stimDur_Fr
+            disp(['#frames recorded:' num2str(nrFramesRecord)]);
+            
+        end
+        
+    end % public methods
+    
+    
+end
---
+++ rdp.m
@@ -0,0 +1,287 @@
+classdef rdp < neurostim.stimulus
+    % Class for drawing a random dot pattern in the PTB.
+    % Code from nsLLDots2 and modified for MATLAB.
+    %
+    % Adjustable variables:
+    %   size - dotsize (px)
+    %   type - 0: square dots (default)
+    %          1: round, anit-aliased dots (favour performance)
+    %          2: round, anti-aliased dots (favour quality)
+    %          3: round, anti-aliased dots (built-in shader)
+    %          4: square dots (built-in shader)
+    %   coordSystem - 0: polar
+    %                 1: cartesian
+    %   speed / direction - dot speed and direction (deg) (if using polar coordinates)
+    %   xspeed / yspeed - dot speed in directions x and y (if using cartesian coordinates)
+    %   nrDots - number of dots
+    %   coherence - dot coherence (0-1)
+    %   motionMode - 0: spiral
+    %                1: linear
+    %   noiseMode - 0: proportion
+    %               1: distribution
+    %   noiseDist - 0: gaussian
+    %               1: uniform
+    %   noiseWidth - width of gaussian/uniform noise.
+    %   lifetime - lifetime of dots (in frames)
+    %   maxRadius - maximum radius of aperture (px)
+    %   dwellTime - parameter for dwelling on frame; e.g. dwellTime = 2
+    %       moves twice as fast.
+    
+    properties (Access=protected)
+        x;
+        y;
+        dx;
+        dy;
+        dR;
+        dphi;
+        radius;
+        phiOffset;
+        framesLeft;
+    end
+    
+    properties
+        deleteMe = 10;
+    end
+    
+    methods (Access = public)
+        function o = rdp(c,name)
+            o = o@neurostim.stimulus(c,name);
+            o.addProperty('size',5,'validate',@isnumeric);
+            o.addProperty('type',0,'validate',@isnumeric);            
+            o.addProperty('maxRadius',100,'validate',@isnumeric);
+            o.addProperty('speed',5,'validate',@isnumeric);
+            o.addProperty('xspeed',0,'validate',@isnumeric);
+            o.addProperty('yspeed',0,'validate',@isnumeric);
+            o.addProperty('direction',0,'validate',@isnumeric);
+            o.addProperty('nrDots',100,'validate',@isnumeric);
+            o.addProperty('coherence',0.5,'validate',@(x)x<=1&&x>=0);
+            o.addProperty('motionMode',1,'validate',@(x)x==0||x==1);      %Spiral, linear
+            o.addProperty('lifetime',50,'validate',@isnumeric);
+            o.addProperty('dwellTime',1,'validate',@isnumeric);
+            o.addProperty('coordSystem',0,'validate',@(x)x==0||x==1);
+            o.addProperty('noiseMode',0,'validate',@(x)x==0||x==1);       % coherence, distribution
+            o.addProperty('noiseDist',0,'validate',@(x)x==0||x==1);       % gaussian, uniform
+            o.addProperty('noiseWidth',50,'validate',@isnumeric);
+            o.addProperty('truncateGauss',-1,'validate',@isnumeric);
+            o.addProperty('rngSeed',[]);
+        end
+        
+        
+        function beforeTrial(o)
+            
+            if ~isempty(o.rngSeed)
+                rng(o.rngSeed);%reset random number generator 
+            end 
+            
+            % overrule one of the velocity vectors based on coord system
+            if o.coordSystem == 1
+                [o.direction, o.speed] = cart2pol(o.xspeed, o.yspeed);
+            else
+                [o.xspeed, o.yspeed] = pol2cart((o.direction.*pi./180), o.speed);
+            end
+            
+            initialiseDots(o,true(o.nrDots,1));
+            
+            % initialise dots' lifetime
+            if o.lifetime ~= Inf
+                o.framesLeft = randi(o.lifetime,o.nrDots,1);
+            else
+                o.framesLeft = ones(o.nrDots,1).*Inf;
+            end
+        end
+        
+        
+        function beforeFrame(o)
+            Screen('DrawDots',o.window, [o.x o.y]', o.size, o.color, [0.0,0.0], o.type);
+        end
+        
+        
+        function afterFrame(o)
+            % reduce lifetime by 1
+            if o.noiseMode ~= 1
+                o.framesLeft = o.framesLeft - 1;
+            end
+            if o.coordSystem == 1
+                [o.direction, o.speed] = cart2pol(o.xspeed, o.yspeed);
+            else
+                [o.xspeed, o.yspeed] = pol2cart((o.direction.*pi./180), o.speed);
+            end
+            moveDots(o);
+            
+            
+        end
+    end
+    
+    methods (Access=protected)
+        % Methods only for personal use
+        
+        function [xnew, ynew] = rotateXY(o,xprev,yprev,arg)
+            % rotates vectors xprev and yprev by angle arg and outputs new
+            % values in xnew and ynew.
+            
+            % create a matrix with all rotation matrices in a column
+            if max(size(xprev))==1
+                R=[cos(arg) -sin(arg);sin(arg) cos(arg)];
+                xynew=R*[xprev;yprev];
+                xnew=xynew(1);
+                ynew=xynew(2);
+            else
+                for a=1:max(size(xprev))
+                    R=[cos(arg(a)) -sin(arg(a));sin(arg(a)) cos(arg(a))];
+                    xynew=R*[xprev(a);yprev(a)];
+                    xnew(a,1)=xynew(1);
+                    ynew(a,1)=xynew(2);
+                end
+            end
+        end
+        
+        
+        function initialiseDots(o,pos)
+            % initialises dots in the array positions given by logical
+            % array pos.
+            
+            if ~isempty(o.rngSeed)
+                rng(o.rngSeed);%reset random number generator 
+            end
+            
+            nnzpos=nnz(pos);
+            o.framesLeft(pos,1) = o.lifetime;
+            o.radius(pos,1) = sqrt(rand(nnzpos,1).*o.maxRadius.*o.maxRadius);
+            randAngle = rand(o.nrDots,1).*360;
+            tmp=ceil(o.coherence*o.nrDots);
+            switch o.motionMode
+                case 0 % spiral
+                    
+                    o.phiOffset(pos,1) = randAngle(pos);
+                    
+                    if tmp>=1 && any(pos(1:tmp))
+                        o.dR(pos(1:tmp),1) = o.xspeed/o.cic.screen.frameRate;
+                        o.dphi(pos(1:tmp),1) = o.xspeed/o.cic.screen.frameRate;
+                    end
+                    if o.coherence == 0 || (o.coherence ~= 1 && any(pos(tmp:end)))
+                        index = find(pos)>=o.coherence*o.nrDots;
+                        o.dR(index,1) = -o.xspeed/o.cic.screen.frameRate;
+                        o.dphi(index,1)=-o.yspeed/o.cic.screen.frameRate;
+                    end
+                    
+                    [o.x(pos,1), o.y(pos,1)] = o.setXY(pos,randAngle);
+                    
+                    
+                case 1  % linear
+                    [o.x(pos,1), o.y(pos,1)] = o.setXY(pos,randAngle);
+                    
+                    switch o.noiseMode
+                        case 0 %proportion
+                            
+                            if (tmp)>=1 && any(pos(1:tmp))
+                                o.dx(pos(1:tmp),1) = o.xspeed./o.cic.screen.frameRate;
+                                o.dy(pos(1:tmp),1) = o.yspeed./o.cic.screen.frameRate;
+                            end
+                            
+                            if o.coherence==1
+                                return;
+                            end
+                            if o.coherence == 0 || any(pos(tmp:end))
+                                index = find(pos)>=o.coherence*o.nrDots;
+                                randAngle(index) = rand(nnz(index),1).*360;
+                                o.dx(index,1) = cosd(randAngle(index)).*o.speed/o.cic.screen.frameRate;
+                                o.dy(index,1) = sind(randAngle(index)).*o.speed/o.cic.screen.frameRate;
+                            end
+                            
+                            
+                        case 1 %distribution
+                            switch o.noiseDist
+                                case 0  %gaussian
+                                    randAngle = o.noiseWidth.*randn(nnz(pos),1);
+                                    if o.truncateGauss ~= -1
+                                        a = abs(randAngle/o.noiseWidth)>o.truncateGauss;
+                                        while max(a)
+                                            randAngle(a) = o.noiseWidth.*randn(sum(a),1);
+                                            a = abs(randAngle/o.noiseWidth)>o.truncateGauss;
+                                        end
+                                    end
+                                case 1 %uniform
+                                    randAngle = rand(nnz(pos),1).*o.noiseWidth - repmat(o.noiseWidth/2, nnz(pos),1);
+                                otherwise
+                                    error('Unknown noiseDist');
+                            end
+                            
+                            randAngle = o.direction + randAngle;
+                            [o.dx(pos,1), o.dy(pos,1)] = pol2cart(randAngle.*(pi./180),o.speed/o.cic.screen.frameRate);
+                        otherwise
+                            error('Unknown noiseMode');
+                            
+                    end
+                otherwise
+                    error('Unknown motionMode');
+            end
+        end
+        
+        function [x,y]=setXY(o,pos,randAngle)
+            x=o.radius(pos).*cosd(randAngle(pos));
+            y=o.radius(pos).*sind(randAngle(pos));
+        end
+        
+        
+        function moveDots(o)
+            
+            %warp - move dots outside aperture
+            
+            %Work with a local copy (faster than repeated "gets" on NS params)
+            dwellTime = o.dwellTime;
+            maxRadius = o.maxRadius;
+            
+            switch o.motionMode
+                case 1 %linear
+
+                    % calculates future position
+                    futureX = o.x+dwellTime.*o.dx;
+                    futureY = o.y+dwellTime.*o.dy;
+                    futureRad = sqrt(futureX.^2 + futureY.^2);
+                    tmp=futureRad>maxRadius;
+                    if any(tmp)   % if any new dots are outside the max radius
+                        % move dots
+                        [dotDir,~] = cart2pol(o.dx(tmp),o.dy(tmp));
+                        [xr, yr] = rotateXY(o,o.x(tmp),o.y(tmp),-dotDir);
+                        chordLength = 2*sqrt(maxRadius^2 - yr.^2);
+                        xr = xr - chordLength;
+                        [o.x(tmp), o.y(tmp)] = rotateXY(o,xr,yr,dotDir);
+                    end
+                    
+                case 0 %spiral
+                    temp1 = o.dR>0 & (o.radius+dwellTime.*o.dR)>maxRadius;
+                    temp2 = o.dR<0 & (o.radius+dwellTime.*o.dR)<0;
+                    
+                    if (any(temp1) || any(temp2))   % if any dots are outside the max radius or below 0
+                        % position new dots randomly
+                        nsMax = max(-1*o.dR(temp1 | temp2), 0);
+                        nsMin = min(maxRadius, maxRadius - o.dR(temp1 | temp2));
+                        o.radius(temp1 | temp2) = nsMax + (nsMin - nsMax).*rand(size(nsMax));
+                    end
+                    
+            end
+            
+            % move dots
+            if any(~o.framesLeft)
+                initialiseDots(o,~o.framesLeft);
+            end
+            
+            switch o.motionMode
+                case 0 %spiral
+                    o.phiOffset = o.phiOffset + o.dwellTime.*o.dphi;
+                    o.radius = o.radius + dwellTime.*o.dR;
+                    o.x = o.radius.*cosd(o.phiOffset);
+                    o.y = o.radius.*sind(o.phiOffset);
+                    
+                case 1  %linear
+                    o.x = o.x + dwellTime.*o.dx;
+                    o.y = o.y + dwellTime.*o.dy;
+            end
+        end
+        
+        
+        
+    end
+    
+    
+end
